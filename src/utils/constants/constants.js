cef7922b1253e91027fa1d83a5ef0f6f28474a2b
bhanu aluka <alukabhanuprakash@gmail.com>
2025-02-21 14:37:00 +0530

    url updated

--- src/utils/constants/constants.js
+++ src/utils/constants/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://nodejs-devtinder-backend-2.onrender.com";
+export const BASE_URL = "https://nodejs-devtinder-backend-4.onrender.com";
 
 // "http://localhost:4002";-- local
 
