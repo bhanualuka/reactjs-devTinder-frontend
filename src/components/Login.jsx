2e23a0e4d017e009e6acc62a9825252a21615bc6
bhanu aluka <alukabhanuprakash@gmail.com>
2025-03-11 16:33:36 +0530

    Added Guest Login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,169 +1,5 @@
-// My code..
-/* 
 import axios from "axios";
-import { use, useState } from "react";
-import { useDispatch } from "react-redux";
-import { addUser } from "../utils/reduxToolkit/slices/userSlice";
-import { useNavigate } from "react-router-dom";
-import { BASE_URL } from "../utils/constants/constants";
-
-const Login = () => {
-  const [emailId, setEmailId] = useState("@gmail.com");
-  const [password, setPassword] = useState("@123");
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [isLogin, setIsLogin] = useState(true);
-  const [error, setError] = useState("");
-
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-
-  const handleLogin = async () => {
-    try {
-      const res = await axios.post(
-        ` ${BASE_URL}/login`,
-        {
-          emailId,
-          password,
-        },
-        {
-          withCredentials: true,
-        }
-      );
-
-      dispatch(addUser(res.data));
-
-      navigate("/");
-    } catch (err) {
-      setError(err?.response?.data || "Something went wrong");
-      console.error(err);
-    }
-  };
-
-  const handleSignUp = async () => {
-    try {
-      const res = await axios.post(
-        `${BASE_URL}/signup`,
-        {
-          firstName,
-          lastName,
-          emailId,
-          password,
-        },
-        {
-          withCredentials: true,
-        }
-      );
-      dispatch(addUser(res.data.data));
-      console.log(res.data.data);
-
-      return navigate("/profile");
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
-  return (
-    <>
-      <div className="flex justify-center my-3 ">
-        <div className="card bg-base-300 w-96 shadow-xl">
-          <div className="card-body">
-            <h2 className="card-title justify-center ">
-              {isLogin ? "Login" : "SignUp"}
-            </h2>
-
-            {!isLogin && (
-              <>
-                {" "}
-                <div className="py-1">
-                  <label className="form-control w-full max-w-xs">
-                    <div className="label"> FirstName</div>
-                    <input
-                      type="text"
-                      placeholder=""
-                      className="input input-bordered w-full max-w-xs"
-                      value={firstName}
-                      onChange={(e) => {
-                        setFirstName(e.target.value);
-                      }}
-                    />
-                  </label>
-                </div>
-                <div className="py-1">
-                  <label className="form-control w-full max-w-xs">
-                    <div className="label"> LastName</div>
-                    <input
-                      type="text"
-                      placeholder=""
-                      className="input input-bordered w-full max-w-xs"
-                      value={lastName}
-                      onChange={(e) => {
-                        setLastName(e.target.value);
-                      }}
-                    />
-                  </label>
-                </div>
-              </>
-            )}
-
-            <div className="py-1">
-              <label className="form-control w-full max-w-xs">
-                <div className="label"> Email Id </div>
-                <input
-                  type="text"
-                  placeholder=""
-                  className="input input-bordered w-full max-w-xs"
-                  value={emailId}
-                  onChange={(e) => {
-                    setEmailId(e.target.value);
-                  }}
-                />
-              </label>
-            </div>
-
-            <div>
-              <label className="form-control w-full max-w-xs">
-                <div className="label"> Password </div>
-                <input
-                  type="password"
-                  placeholder=""
-                  className="input input-bordered w-full max-w-xs"
-                  value={password}
-                  onChange={(e) => {
-                    setPassword(e.target.value);
-                  }}
-                />
-              </label>
-            </div>
-
-            <p className="text-red-800 flex justify-center"> {error} </p>
-            <div className="card-actions justify-center py-2 ">
-              <button
-                className="btn btn-primary"
-                onClick={isLogin ? handleLogin : handleSignUp}
-              >
-                {isLogin ? "Login" : "SignUp"}
-              </button>
-            </div>
-
-            <p
-              className="m-auto cursor-pointer"
-              onClick={() => setIsLogin((value) => !value)}
-            >
-              {isLogin ? "New User SignUp!" : "Exisiting User.. Login In"}
-            </p>
-          </div>
-        </div>
-      </div>
-    </>
-  );
-};
-
-export default Login;
- */
-
-import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useDispatch } from "react-redux";
 import { addUser } from "../utils/reduxToolkit/slices/userSlice";
 import { useNavigate } from "react-router-dom";
@@ -171,29 +7,27 @@ import { BASE_URL } from "../utils/constants/constants";
 import { useCookies } from "react-cookie";
 
 const Login = () => {
-  const [emailId, setEmailId] = useState("");
-  const [password, setPassword] = useState("");
+  // const [emailId, setEmailId] = useState("");
+  // const [password, setPassword] = useState("");
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [isLogin, setIsLogin] = useState(true);
   const [error, setError] = useState("");
   const [cookies, setCookie] = useCookies(["Token"]);
+  const emailref = useRef(null);
+  const passwordref = useRef(null);
 
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
-  const handleGuest = async () => {
-    setEmailId("test@gmail.com");
-    setPassword("Test@123");
-  };
-
+  // Login
   const handleLogin = async () => {
     try {
       const res = await axios.post(
         `${BASE_URL}/login`,
         {
-          emailId,
-          password,
+          emailId: emailref.current.value,
+          password: passwordref.current.value,
         },
         {
           withCredentials: true,
@@ -210,6 +44,7 @@ const Login = () => {
     }
   };
 
+  // SignUp
   const handleSignUp = async () => {
     try {
       const res = await axios.post(
@@ -217,31 +52,49 @@ const Login = () => {
         {
           firstName,
           lastName,
-          emailId,
-          password,
+          emailId: emailref.current.value,
+          password: passwordref.current.value,
         },
         {
           withCredentials: true,
         }
       );
+
       dispatch(addUser(res.data.data));
 
-      setCookie("Token", res.data.token, { path: "/" });
-      console.log(res.data.data);
-      console.log(cookies);
-      navigate("/");
+      setCookie("Token", cookies.token, { path: "/" });
+      // console.log(res.data.token, "Token");
+      setIsLogin(true);
+      emailref.current.value = "";
+      passwordref.current.value = "";
     } catch (err) {
       console.error(err);
     }
   };
 
-  useEffect(() => {
-    if (cookies.Token) {
+  // Guest login(default)
+  const handleGuest = async () => {
+    try {
+      const res = await axios.post(
+        `${BASE_URL}/login`,
+        {
+          emailId: "test3@gmail.com",
+          password: "test@123",
+        },
+        {
+          withCredentials: true,
+        }
+      );
+
+      dispatch(addUser(res.data.user));
+      setCookie("Token", res.data.token, { path: "/" });
+
       navigate("/");
-    } else if (emailId === "test@gmail.com" && password === "Test@123") {
-      handleLogin();
+    } catch (err) {
+      setError(err?.response?.data || "Something went wrong");
+      console.error(err);
     }
-  }, [cookies.Token, emailId, password]);
+  };
 
   return (
     <>
@@ -290,10 +143,7 @@ const Login = () => {
                   type="text"
                   placeholder=""
                   className="input input-bordered w-full max-w-xs sm:max-w-md"
-                  value={emailId}
-                  onChange={(e) => {
-                    setEmailId(e.target.value);
-                  }}
+                  ref={emailref}
                 />
               </label>
             </div>
@@ -304,10 +154,7 @@ const Login = () => {
                   type="password"
                   placeholder=""
                   className="input input-bordered w-full max-w-xs sm:max-w-md"
-                  value={password}
-                  onChange={(e) => {
-                    setPassword(e.target.value);
-                  }}
+                  ref={passwordref}
                 />
               </label>
             </div>
@@ -345,6 +192,3 @@ const Login = () => {
 };
 
 export default Login;
-
-/*
- */
--- src/utils/constants/constants.js
+++ src/utils/constants/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:8084";
+export const BASE_URL = "http://localhost:8094";
 
 // "http://localhost:4002";-- local
 
