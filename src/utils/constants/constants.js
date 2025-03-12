7b8ad2c669666f9f85ec3c59af07cacc4efacf7e
bhanu aluka <alukabhanuprakash@gmail.com>
2025-03-12 11:28:03 +0530

    added url

--- src/utils/constants/constants.js
+++ src/utils/constants/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://nodejs-devtinder-backend-5.onrender.com";
+export const BASE_URL = "https://nodejs-devtinder-backend-6.onrender.com";
 
 // "http://localhost:4002";-- local
 
